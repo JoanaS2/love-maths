3fb19f46b0fc3f787117ece0ddadaead23dbf39e
Joana Silva <jslvag@outlook.com>
2023-11-10 17:45:10 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-
+    } else if (gameType === "mulitply"){
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknwon game type: ${gameType}. Aborting!`;
@@ -71,6 +72,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addititon"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -111,6 +114,10 @@ function displaySubractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "x";
 
 }
