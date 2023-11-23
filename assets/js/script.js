ebd4fb1d759e94524cde1f8dd901e5692951ffc2
Joana Silva <jslvag@outlook.com>
2023-11-23 17:36:24 +0000

    Add subtraction gametype to run game function, create display question function and modify calculate correct answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,8 +30,10 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else if (gameType === "mulitply"){
+    } else if (gameType === "multiply"){
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract"){
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknwon game type: ${gameType}. Aborting!`;
@@ -74,6 +76,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addititon"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -110,8 +114,10 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubractQuestion() {
-
+function displaySubtractQuestion(operand1 , operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById(`operator`).textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
